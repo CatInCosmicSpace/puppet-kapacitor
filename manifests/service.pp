f3d3e1660e73051bdc0702c304b5efd894b1fd63
CatInCosmicSpace <catincosmicspace@gmail.com>
2022-01-13 18:41:09 +0300

    Returned formatting of service.pp
    Signed-off-by: CatInCosmicSpace <catincosmicspace@gmail.com>

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,13 +3,13 @@
 # @example
 #   include kapacitor::service
 class kapacitor::service (
-  String $service_name                     = $kapacitor::service_name,
-  Stdlib::Ensure::Service $service_ensure  = $kapacitor::service_ensure,
-  Boolean $service_enable                  = $kapacitor::service_enable,
-  Boolean $service_has_status              = $kapacitor::service_has_status,
-  Boolean $service_has_restart             = $kapacitor::service_has_restart,
-  String $service_provider                 = $kapacitor::service_provider,
-  Boolean $manage_service                  = $kapacitor::manage_service,
+  String $service_name = $kapacitor::service_name,
+  Stdlib::Ensure::Service $service_ensure = $kapacitor::service_ensure,
+  Boolean $service_enable = $kapacitor::service_enable,
+  Boolean $service_has_status = $kapacitor::service_has_status,
+  Boolean $service_has_restart = $kapacitor::service_has_restart,
+  String $service_provider = $kapacitor::service_provider,
+  Boolean $manage_service = $kapacitor::manage_service,
   Stdlib::Absolutepath $service_definition = $kapacitor::service_definition,
 ) {
   if $manage_service and $kapacitor::ensure != 'absent' {
